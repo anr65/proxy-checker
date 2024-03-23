df81d60251967aeceb486663e82e0b7970064d43
anr65 <armuradov@yahoo.com>
2024-03-23 18:01:18 +0300

    testing first version 2. edited view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -60,6 +60,8 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
 <script>
     $(document).ready(function() {
         $('#proxyForm').submit(function(event) {
