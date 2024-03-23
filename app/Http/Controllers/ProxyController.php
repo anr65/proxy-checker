8fc634c2299ad720b34e0a8fec5cfd1f933b052f
anr65 <armuradov@yahoo.com>
2024-03-23 17:47:33 +0300

    testing first version 2. edited view

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class ProxyController extends Controller
@@ -35,47 +36,18 @@ class ProxyController extends Controller
         $ip = $proxyParts[0];
         $port = $proxyParts[1];
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://google.com'); // Замените example.com на адрес, который вы хотите запросить через прокси
-        curl_setopt($ch, CURLOPT_PROXY, $ip);
-        curl_setopt($ch, CURLOPT_PROXYPORT, $port);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_NOBODY, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Опционально: установка таймаута для запроса
-        $response = curl_exec($ch);
-        $errorCode = curl_errno($ch);
-        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $headers = substr($response, 0, $headerSize);
-        curl_close($ch);
+        $client = new Client();
+        $response = $client->get("http://ip-api.com/json/{$ip}?fields=country,city,isp");
+        $locationData = json_decode($response->getBody()->getContents(), true);
 
         $proxyInfo = [
             'ip' => $ip,
             'port' => $port,
-            'status' => 'not working',
-            'type' => '', // Вы можете определить тип прокси здесь, если это необходимо
-            'country' => '', // Если требуется, определите страну и город прокси
-            'city' => '',
-            'download_speed' => '', // Дополнительные данные о прокси, такие как скорость скачивания, могут быть добавлены здесь
-            'external_ip' => ''
+            'status' => 'unknown',
+            'country' => $locationData['country'] ?? 'Unknown',
+            'city' => $locationData['city'] ?? 'Unknown',
+            'isp' => $locationData['isp'] ?? 'Unknown'
         ];
-
-        $proxyInfo['status'] = 'working';
-
-        // Определение типа прокси
-        $proxyInfo['type'] = strpos($headers, 'SOCKS') !== false ? 'socks' : 'http';
-
-        // Парсинг заголовков для определения страны и города
-        preg_match('/X-Country: ([A-Za-z]+)/', $headers, $matches);
-        if (!empty($matches[1])) {
-            $proxyInfo['country'] = $matches[1];
-        }
-
-        // Получаем внешний IP прокси
-        preg_match('/X-Forwarded-For: ([\d.]+)/', $headers, $matches);
-        if (!empty($matches[1])) {
-            $proxyInfo['external_ip'] = $matches[1];
-        }
 
         return $proxyInfo;
     }
