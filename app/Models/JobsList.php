1149b2d0e8b84852fc95920e277fd36a62028961
anr65 <armuradov@yahoo.com>
2024-03-23 20:02:22 +0300

    added job list handle

--- app/Models/JobsList.php
+++ app/Models/JobsList.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class JobsList extends Model
 {
     use HasFactory;
+
+    protected $table = 'jobs_list';
+
+    protected $fillable = [
+        'id',
+        'started_at',
+        'ended_at',
+        'total_count',
+        'working_count'
+    ];
 }
--- database/migrations/2024_03_23_165328_create_jobs_list_table.php
+++ database/migrations/2024_03_23_165328_create_jobs_list_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('jobs_list', function (Blueprint $table) {
             $table->id();
-            $table->string('started_at')->nullable();
             $table->timestamp('started_at')->nullable();
             $table->timestamp('ended_at')->nullable();
             $table->integer('total_count')->nullable();
