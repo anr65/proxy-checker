82dc1ac76d8518092751aa72d39e9597578c3ec5
anr65 <armuradov@yahoo.com>
2024-03-23 20:45:12 +0300

    added favicon

--- resources/views/proxy-history.blade.php
+++ resources/views/proxy-history.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proxy Checker - История</title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
