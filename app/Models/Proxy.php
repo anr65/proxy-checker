4c54daf63c2862fff747ae2de467c75dc702cea1
anr65 <armuradov@yahoo.com>
2024-03-23 16:11:27 +0300

    filled Proxy model

--- app/Models/Proxy.php
+++ app/Models/Proxy.php
@@ -4,12 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-$table->string('ip_port')->nullable();
-$table->string('type')->nullable();
-$table->string('location')->nullable();
-$table->boolean('status')->nullable();
-$table->integer('timeout')->nullable();
-$table->string('ext_ip')->nullable();
 class Proxy extends Model
 {
     use HasFactory;
