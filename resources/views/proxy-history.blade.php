9e656b9240d04e82bec437f48935a98e80362c18
anr65 <armuradov@yahoo.com>
2024-03-23 20:33:04 +0300

    fix on modal job id

--- resources/views/proxy-history.blade.php
+++ resources/views/proxy-history.blade.php
@@ -18,7 +18,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link active" href="#home">Главная</a>
+                <a class="nav-link active" href="/">Главная</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/proxy-history">История</a> <!-- Add link to История page -->
