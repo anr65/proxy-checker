2f7264efc0436296b3aa37f639c5d29b628d2688
anr65 <armuradov@yahoo.com>
2024-03-24 17:41:59 +0300

    Fix on polling logic 2

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -131,7 +131,7 @@
                     url: '/check-proxies/progress',
                     data: { uuid: jobId },
                     success: function(response) {
-                        if (response.success && response.done === 1) {
+                        if (response.done === 1) {
                             clearInterval(intervalId);
                             displayResults(response);
                             loading.hide();
